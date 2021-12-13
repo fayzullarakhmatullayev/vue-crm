064cc90fd73a31c85f2a4bb181e3268eedf8a775
Fayzulla <fayzullarakhmatullayev@gmail.com>
2021-12-13 10:36:14 +0500

    New Deployment

---
+++ precache-manifest.303696f8af79677e4602c28ffd613e95.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0865820829e68907fb",
+    "url": "/vue-crm/css/app.3331ac6c.css"
+  },
+  {
+    "revision": "5daa49604425a946158f",
+    "url": "/vue-crm/css/chunk-c8ba844a.3dcc7f59.css"
+  },
+  {
+    "revision": "2b1655cedd2d1e498c9a",
+    "url": "/vue-crm/css/chunk-vendors.d2a37789.css"
+  },
+  {
+    "revision": "16178e2efa44681c987298fd2d362dc9",
+    "url": "/vue-crm/index.html"
+  },
+  {
+    "revision": "1f0865820829e68907fb",
+    "url": "/vue-crm/js/app.b1d0bdfc.js"
+  },
+  {
+    "revision": "b851552d66ce8052778c",
+    "url": "/vue-crm/js/chunk-10b86dd9.0dd3bdc5.js"
+  },
+  {
+    "revision": "cf4a9e262b97209f27cf",
+    "url": "/vue-crm/js/chunk-2d0d05dc.d46998cc.js"
+  },
+  {
+    "revision": "06c3dbf2d0190ced5acb",
+    "url": "/vue-crm/js/chunk-2d0d7699.4f7e0118.js"
+  },
+  {
+    "revision": "d325dc4502f015e3bf8f",
+    "url": "/vue-crm/js/chunk-2d21a3d2.904c1d19.js"
+  },
+  {
+    "revision": "28b95709dfa36167939f",
+    "url": "/vue-crm/js/chunk-53c8ec65.3a38db8c.js"
+  },
+  {
+    "revision": "d3343afba7bbf926cb08",
+    "url": "/vue-crm/js/chunk-5f4ad635.d2fa8312.js"
+  },
+  {
+    "revision": "185b320ba25000780bb0",
+    "url": "/vue-crm/js/chunk-bc3311b2.0e3c402e.js"
+  },
+  {
+    "revision": "4a4b1761158f498ab51e",
+    "url": "/vue-crm/js/chunk-c420df12.7fb06b5f.js"
+  },
+  {
+    "revision": "5daa49604425a946158f",
+    "url": "/vue-crm/js/chunk-c8ba844a.eedc00fd.js"
+  },
+  {
+    "revision": "2b1655cedd2d1e498c9a",
+    "url": "/vue-crm/js/chunk-vendors.6276969e.js"
+  },
+  {
+    "revision": "6a8d2c9e18169fece64b94593566de0d",
+    "url": "/vue-crm/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-crm/robots.txt"
+  }
+]);
